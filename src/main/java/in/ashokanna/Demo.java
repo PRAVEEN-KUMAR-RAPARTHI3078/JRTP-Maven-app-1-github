3705f675fdf9e0fc0186d9ec089666894f2ff8e6
Ashok Anna <praveenitcourse@gmail.com>
2023-03-23 12:06:55 +0530

    Bug-7994 fixed

--- src/main/java/in/ashokanna/Demo.java
+++ src/main/java/in/ashokanna/Demo.java
@@ -1,6 +1,6 @@
 package in.ashokanna;
 
-public class Demo {
+public class Demo extends RuntimeException {
 	
 	public static void main(String[] args) {
 		System.out.print("Hello Welcome to Github....!");
